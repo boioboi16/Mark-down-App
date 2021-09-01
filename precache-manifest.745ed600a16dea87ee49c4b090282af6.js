1e97d9c31f1678b91ebfbf287ddb0410e6949ddc
boioboi16 <aaryan.praveen@gmail.com>
2021-09-01 23:16:18 +0530

    Updates

---
+++ precache-manifest.745ed600a16dea87ee49c4b090282af6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "31ad930f67b37a00557cd0c0ba33e3ab",
+    "url": "/markdown-app/index.html"
+  },
+  {
+    "revision": "b68028cb406e243ad8f7",
+    "url": "/markdown-app/static/css/main.67d641af.chunk.css"
+  },
+  {
+    "revision": "6bbb2575db765f6d65f2",
+    "url": "/markdown-app/static/js/2.9d0b99b7.chunk.js"
+  },
+  {
+    "revision": "aef280276b8d6f56e9d8dc46988119d9",
+    "url": "/markdown-app/static/js/2.9d0b99b7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b68028cb406e243ad8f7",
+    "url": "/markdown-app/static/js/main.889497a8.chunk.js"
+  },
+  {
+    "revision": "6294607c673cceadeaa5",
+    "url": "/markdown-app/static/js/runtime-main.e75901f8.js"
+  }
+]);
